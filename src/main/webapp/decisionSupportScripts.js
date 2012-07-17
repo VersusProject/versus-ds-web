21ec65dfaa443a8ac31d84c1975da9c67f9625fa
dbonnie <dbonnie@ncsa.illinois.edu>
2012-07-17 09:57:59 -0500

    Website is currently in mock-up, other minor fixes.

--- src/main/webapp/decisionSupportScripts.js
+++ src/main/webapp/decisionSupportScripts.js
@@ -2,13 +2,16 @@ var fileNum           = 0;
 var adapterState      = false;
 var radioButtonsState = false;
 var fileList;
-var adapterList;
+var adapterList = [];
 var radioPlusCount    = 0;
 var radioMinusCount   = 0;
 var loadingState      = 0;
 var adapterMimeType;
 var similarFileUrls;
 var dissimilarFileUrls;
+var results;
+var dsId = [];
+var resultsState = false;
 //change plus radio button icon
 function displayPlus(clickedID){ 
 	var radioNumber = clickedID.substr(clickedID.lastIndexOf("s")+1,clickedID.length-1);	
@@ -30,22 +33,25 @@ function toggleSubmitButton(){
 		$("#submitButton").attr("class","btn btn-primary disabled");
 	}
 }
-//check to see if the adapter has been set to a valid choice (onclick function)
-function setAdapterState(){
-	if(  $("#adapterDropDown").val() == "invalid"){
-		adapterState = false;
-	}
-	else{
-		adapterState    = true;
-		for( var i=0; i<adapterList.length; i++){
-			if($("#adapterDropDown").val() == adapterList[i].id ){
-				adapterMimeType = adapterList[i].mimeTypes;
-			}
-		}
-		console.log(adapterMimeType);
-	}
-	toggleSubmitButton();
+
+function uploadFile(){
+	
+	var data = new FormData();
+	data.append("file-1",fileList[0]);
+	
+	$.ajax({
+	    url: 'http://localhost:8080/api/v1/files/upload',
+	    data: data,
+	    cache: false,
+	    contentType: 'multipart/form-data',
+	    processData: false,
+	    type: 'POST',
+	    success: function(data){
+	        alert(data);
+	    }
+	});
 }
+
 //check all radio buttons, if there is at least one set of (+/-) unchecked, then radioButtonState is false
 function checkRadioButtons(){
 	
@@ -186,44 +192,83 @@ function checkNumLabels(){
 		}
 	}
 }
-//set the global var adapterList: {'id':'adaptId', 'name':'name', 'mimeTypes':['application/xml', 'text/html']}
-function getAdapters() {
-	adapterList = [
-	               {'id':'RGB', 'name':'RGB Adapter', 'mimeTypes':['image/gif', 'image/jpg','image/jpeg','image/png']},
-	               {'id':'Audio', 'name':'Audio Adapter', 'mimeTypes':['audio/mpeg', 'audio/mp4']},
-	               {'id':'Video', 'name':'Video Adapter', 'mimeTypes':['video/mpeg', 'video/quicktime']}
-	];
-}
-//set the adapter options, pulled from the versus server
-$(document).ready(function setAvailableAdapters(){
-	getAdapters();//global car adapterList is now set
-	var availableAdapters = "";
-	for(var i=0; i<adapterList.length; i++){
-		availableAdapters = availableAdapters+'<option value="'+adapterList[i].id+'">'+adapterList[i].name+'</option>';
-	}
-	$("#adapterDropDown").html('<option value="invalid">--Adapter--</option>'+availableAdapters);
-});
+
 
 function submitDecisionSupport(){
 	
 	var chosenAdapter  = "edu.illinois.ncsa.versus.adapter.impl.BufferedImageAdapter";
-	similarFileUrls    = ["http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-18.jpg","http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-7.jpg"];
-	dissimilarFileUrls = ["http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-25.jpg","http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-16.jpg"];
-	
-	
-	
+	//similarFileUrls    = ["http://www.cs.washington.edu/research/imagedatabase/groundtruth/greenlake/Image01.jpg","http://www.cs.washington.edu/research/imagedatabase/groundtruth/greenlake/Image02.jpg"];
+	//dissimilarFileUrls = ["http://www.cs.washington.edu/research/imagedatabase/groundtruth/swissmountains/Image01.jpg","http://www.cs.washington.edu/research/imagedatabase/groundtruth/swissmountains/Image03.jpg"];
+
+	similarFileUrls    = ["http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-5.jpg","http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-18.jpg"];
+	dissimilarFileUrls = ["http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-16.jpg","http://isda.ncsa.illinois.edu/drupal/sites/default/files/pictures/picture-25.jpg"];
+
 	//post the data
 	$.ajax({
 		type: 'POST',
-		url: 'http://localhost:8080/versus-ds-web/api/v1/decisionSupport',
+		url: 'http://localhost:8080/api/v1/decisionSupport',
 		data: "adapter="+chosenAdapter+
 			"&similarFiles="+similarFileUrls[0]+
 			"&similarFiles="+similarFileUrls[1]+
 			"&dissimilarFiles="+dissimilarFileUrls[0]+
 			"&dissimilarFiles="+dissimilarFileUrls[1]		
+	}).done( function( id ) {
+		$("#results").html('<br> <br> <br> <br> <br> <br> <div id="loadingBar" class="progress progress-striped active"><div class="bar"style="width: 100%;"></div></div><br> <br> <br> <br> <br>');
+		getDecisionSupport(id);
 	});
 }
 
+function getDecisionSupport(id){
+	$.getJSON('http://localhost:8080/api/v1/decisionSupport/'+id, function(json) {
+		formatAndDisplayResults(json);
+	});
+}
+
+function formatAndDisplayResults(j){
+	
+	$("#status").html(j.status);
+
+	//if status is not running then display results
+	if(j.status == "DONE"){
+		$("#status").remove();
+		$("#loadingBar").remove();
+		
+		var results = "<b>"+j.rankedResults.substring(0,j.rankedResults.indexOf("2.)")-1)+"</b><br><br>"+
+			j.rankedResults.substring( j.rankedResults.indexOf("2.)"), j.rankedResults.indexOf("3.)")-1 )+"<br><br>"+
+			j.rankedResults.substring( j.rankedResults.indexOf("3.)"), j.rankedResults.indexOf("4.)")-1 )+"<br><br>"+
+			j.rankedResults.substring( j.rankedResults.indexOf("4.)"), j.rankedResults.length );
+
+		$("#results").html("<br/><u>The Best Extractor / Measure Pairs are:</u> <br><br>"+results);
+		resultsState = true;
+	}
+	//if status has error then display error
+	else if(j.status == "FAILED"){
+		$("#status").remove();
+
+		$("#loadingBar").remove();
+		$("#results").append("Failed!");
+		resultsState = true;
+	}
+}
+
+function getAdapters() {
+	$.getJSON('http://localhost:8080/api/v1/adapters/', function(json){
+		setAdapterList(json);
+	});
+}
+//set the adapter options, pulled from the versus server
+$(document).ready(function setAvailableAdapters(){
+	getAdapters();//global car adapterList is now set
+});
+
+function setAdapterList(json){
+	var availableAdapters = "";
+	for(var i=0; i<json.length; i++){
+		availableAdapters  = availableAdapters+'<option value="'+json[i].id+'" >'+json[i].name+'</option>';
+		adapterList[i] = {id:json[i].id,mimeTypes:json[i].supportedMimeTypes}; 
+	}
+	$("#adapterDropDown").html('<option value="invalid">--Adapter--</option>'+availableAdapters);
+}
 
 function validateAdapter(){
 	//get the current setting of the adapter selector
@@ -246,4 +291,21 @@ function validateAdapter(){
 	else{
 		return false;
 	}
+}
+
+//check to see if the adapter has been set to a valid choice (onclick function)
+function setAdapterState(){
+	if(  $("#adapterDropDown").val() == "invalid"){
+		adapterState = false;
+	}
+	else{
+		adapterState    = true;
+		for( var i=0; i<adapterList.length; i++){
+			if($("#adapterDropDown").val() == adapterList[i].id ){
+				adapterMimeType = adapterList[i].mimeTypes;
+			}
+		}
+		console.log(adapterMimeType);
+	}
+	toggleSubmitButton();
 }
